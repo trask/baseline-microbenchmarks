5f8c6d921c27685b275de3288f5db1a7b0d03c59
Trask Stalnaker <trask.stalnaker@gmail.com>
2014-07-12 13:07:40 -0700

    Initial commit

---
+++ src/main/java/com/github/trask/microbenchmarks/ReflectionBenchmark.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.trask.microbenchmarks;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+/**
+ * @author Trask Stalnaker
+ * @since 0.5
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+public class ReflectionBenchmark {
+
+    private Method method;
+    private MethodHandle methodHandle;
+
+    @Setup
+    public void setup() throws Exception {
+        method = String.class.getMethod("toString");
+        method.setAccessible(true);
+
+        Lookup lookup = MethodHandles.lookup();
+        MethodType mt = MethodType.methodType(int.class);
+        methodHandle = lookup.findVirtual(String.class, "toString", mt);
+    }
+
+    @Benchmark
+    public Object invoke() throws Throwable {
+        return method.invoke("");
+    }
+
+    @Benchmark
+    public Object invokeUsingMethodHandle() throws Throwable {
+        return methodHandle.invokeExact("");
+    }
+
+    @Benchmark
+    public Object direct() throws Throwable {
+        return "".toString();
+    }
+}
